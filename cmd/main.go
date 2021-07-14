b114e95a8c8878ae39906939e71aa11b7e8a953e
Martín Montes <martin11lrx@gmail.com>
2021-07-14 22:02:29 +0200

    feat: creating CRD using extensions api

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,46 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"os"
+
+	gs "github.com/gotway/gotway/pkg/gracefulshutdown"
+	"github.com/gotway/gotway/pkg/log"
+	crd "github.com/mmontes11/echoperator/pkg/kubernetes/echoperator"
+
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	logger := log.NewLogger(log.Fields{
+		"service": "echoperator",
+	}, "local", "debug", os.Stdout)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var config *rest.Config
+	var err error
+	if kubeconfig, ok := os.LookupEnv("KUBECONFIG"); ok {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+	} else {
+		config, err = rest.InClusterConfig()
+	}
+	if err != nil {
+		logger.Fatal("error getting kubernetes config ", err)
+	}
+
+	apiextensionsClientSet, err := apiextensionsclient.NewForConfig(config)
+	if err != nil {
+		logger.Fatal("error creating api extensions client ", err)
+	}
+
+	logger.Info("creating curstom resource definition")
+	_, err = crd.CreateCustomResourceDefinition(ctx, apiextensionsClientSet)
+	if err != nil {
+		logger.Fatal("error creating custom resource definition ", err)
+	}
+
+	gs.GracefulShutdown(logger, cancel)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mmontes11/echoperator
 go 1.15
 
 require (
+	github.com/gotway/gotway v0.0.11
 	k8s.io/apiextensions-apiserver v0.21.2
 	k8s.io/apimachinery v0.21.2
 	k8s.io/client-go v0.21.2
