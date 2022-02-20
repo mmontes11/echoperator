badaca4cc07cb4b39153d5761144281fd6ffe31a
Martín Montes <martin11lrx@gmail.com>
2022-02-20 20:51:28 +0100

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,10 @@ set -e
 repo="mmontes"
 release="echoperator"
 chart="$repo/$release"
-
-git fetch --all
-tag=$(git describe --abbrev=0 --tags)
+namespace="echoperator"
 
 helm repo add "$repo" https://charts.mmontes-dev.duckdns.org
 helm repo update
 
 echo "🚀 Deploying '$chart' with image version '$tag'..."
-helm upgrade --install "$release" "$chart" --set image.tag=$tag --namespace echoperator
+helm upgrade --install "$release" "$chart" --namespace "$namespace"
